c19289f5a4373a3e5700ca7bad16279d56724e3a
Nasser Ali <nasserali60@live.com>
2024-10-23 13:02:59 -0230

    Set up constructor, getters, and setters for Patron class as well as Student and Employee children

--- src/main/patrons/Student.java
+++ src/main/patrons/Student.java
@@ -1,13 +1,18 @@
 package main.patrons;
 
 public class Student extends Patron {
-    private int id; // Initalize variable id to only represent id of Student (by breaking up classes we can individalize objects like id)
+    private int id; // Initalize variable id to only represent id of Student (by breaking up classes
+                    // we can individalize objects like id)
     // For example, the set of student id's will be different from the employee id's
-    // student id = 100 will not also represent an employee id = 100, thus we can have a unique set of id's for each class
-    // Removes the need to have to implement a check to see if the id is unique across all patrons
+    // student id = 100 will not also represent an employee id = 100, thus we can
+    // have a unique set of id's for each class
+    // Removes the need to have to implement a check to see if the id is unique
+    // across all patrons
     // Somewhat overkill for this project but it is a good practice to follow in OOP
-    public Student(int id) {
-        super(); // Call the constructor of the superclass Student to initialize the Student fields
+
+    public Student(String name, String address, String phoneNumber, int id) {
+        super(name, address, phoneNumber); // Call the constructor of the superclass Student to initialize the Student
+                                           // fields
         this.id = id; // Set the id of the Student
     }
 }
