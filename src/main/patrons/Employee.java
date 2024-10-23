0be280d246e2aed338ae5ed5ef91345e374bcfb9
Nasser Ali <nasserali60@live.com>
2024-10-23 13:00:01 -0230

    Set up constructor, getters, and setters for Patron class as well as Student and Employee children

--- src/main/patrons/Employee.java
+++ src/main/patrons/Employee.java
@@ -1,12 +1,12 @@
 package main.patrons;
 
-
 public class Employee extends Patron {
     private int id; // Initalize variable id to only represent id of Employee
 
-    public Employee(int id) {
-        super(); // Call the constructor of the superclass Employee to initialize the Employee fields
+    public Employee(String name, String address, String phoneNumber, int id) {
+        super(name, address, phoneNumber); // Call the constructor of the superclass Employee to initialize the Employee
+                                           // fields
         this.id = id; // Set the id of the Employee
     }
-    
+
 }
--- src/main/patrons/Patron.java
+++ src/main/patrons/Patron.java
@@ -1,5 +1,43 @@
 package main.patrons;
 
 public abstract class Patron {
-    
+    protected String name;
+    protected String address;
+    protected String phoneNumber;
+    protected ArrayList<LibraryItem> borrowedLibraryItems;
+
+    public Patron(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+        borrowedLibraryItems = new ArrayList<LibraryItem>();
+    }
+
+    public void borrowLibraryItem(LibraryItem libraryItem) {
+        borrowedLibraryItems.add(libraryItem);
+    }
+
+    public void returnLibraryItem(LibraryItem libraryItem) {
+        borrowedLibraryItems.remove(libraryItem);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public ArrayList<LibraryItem> getBorrowedLibraryItems() {
+        return borrowedLibraryItems;
+    }
+
+    public String toString() {
+        return "Name: " + name + "\nAddress: " + address + "\nPhone Number: " + phoneNumber;
+    }
 }
