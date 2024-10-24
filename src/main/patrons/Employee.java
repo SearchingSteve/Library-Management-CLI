048b9d226d0e1703922d463686cb04a1774f8563
Nasser Ali <nasserali60@live.com>
2024-10-24 17:33:00 -0230

    fix: reworked the class logic for the patron class and its student/employee children

--- src/main/patrons/Employee.java
+++ src/main/patrons/Employee.java
@@ -1,12 +1,36 @@
 package main.patrons;
 
 public class Employee extends Patron {
-    private int id; // Initalize variable id to only represent id of Employee
+    private static int employeeIdCounter = 100; // Counter to keep track of the id of the Employee
+    // private int employeeId;
+    // Initalize variable id to only represent id of Employee
 
-    public Employee(String name, String address, String phoneNumber, int id) {
-        super(name, address, phoneNumber); // Call the constructor of the superclass Employee to initialize the Employee
-                                           // fields
-        this.id = id; // Set the id of the Employee
+    public Employee(String employeeId, String name, String address, String phoneNumber) {
+        super("E" + employeeIdCounter, name, address, phoneNumber); // Call the constructor of the superclass Student to
+                                                                    // initialize the Employee fields
+        employeeIdCounter++;
+    }
+
+    public void editEmployee(String name, String address, String phoneNumber) {
+        super.editPatron(name, address, phoneNumber);
+    }
+
+    public void deleteEmployee() {
+        super.deletePatron();
+    }
+
+    public String getEmployeeId() {
+        return this.getId();
+    }
+
+    @Override
+    public String getType() {
+        return "Employee";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nEmployee ID: " + this.getId();
     }
 
 }
--- src/main/patrons/Student.java
+++ src/main/patrons/Student.java
@@ -1,8 +1,10 @@
 package main.patrons;
 
 public class Student extends Patron {
-    private int id; // Initalize variable id to only represent id of Student (by breaking up classes
-                    // we can individalize objects like id)
+    private static int studentIdCounter = 100; // Counter to keep track of the id of the Student
+    // // private int studentId;
+    // Initalize variable id to only represent id of Student (by breaking up classes
+    // we can individalize objects like id)
     // For example, the set of student id's will be different from the employee id's
     // student id = 100 will not also represent an employee id = 100, thus we can
     // have a unique set of id's for each class
@@ -10,9 +12,31 @@ public class Student extends Patron {
     // across all patrons
     // Somewhat overkill for this project but it is a good practice to follow in OOP
 
-    public Student(String name, String address, String phoneNumber, int id) {
-        super(name, address, phoneNumber); // Call the constructor of the superclass Student to initialize the Student
-                                           // fields
-        this.id = id; // Set the id of the Student
+    public Student(String studentId, String name, String address, String phoneNumber) {
+        super("S" + studentIdCounter, name, address, phoneNumber); // Call the constructor of the superclass Student to
+                                                                   // initialize the Student fields
+        studentIdCounter++;
+    }
+
+    public void editStudent(String name, String address, String phoneNumber) {
+        super.editPatron(name, address, phoneNumber);
+    }
+
+    public void deleteStudent() {
+        super.deletePatron();
+    }
+
+    public String getStudentId() {
+        return this.getId();
+    }
+
+    @Override
+    public String getType() {
+        return "Student";
+    }
+
+    @Override
+    public String toString() {
+        return super.toString() + "\nStudent ID: " + this.getId();
     }
 }
