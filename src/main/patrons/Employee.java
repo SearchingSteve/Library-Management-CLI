8cf019812ecfb2331ca30639d600599a2c86b7aa
sara woodford <sarawoodford6@gmail.com>
2024-10-30 19:20:36 -0230

    docs: Employee and Student
    added javadocs for Patron subclasses (employee and
    student)

--- src/main/patrons/Employee.java
+++ src/main/patrons/Employee.java
@@ -1,8 +1,22 @@
 package main.patrons;
 // employee is a subclass of Patron
+
+/**
+ * Employee class is a subclass of {@link Patron} that represents an employee patron 
+ * in the library system. It inherits all attributes and methods from Patron.
+ */
 public class Employee extends Patron {
 
     // CONSTRUCTOR
+
+    /**
+     * Constructs a new Employee object with the specified attributes.
+     * 
+     * @param employeeId   unique identifier for the employee
+     * @param name         name of the employee
+     * @param address      address of the employee
+     * @param phoneNumber  phone number of the employee
+     */
     public Employee(String employeeId, String name, String address, String phoneNumber) {
         // include all params of patron
         super("E" + employeeId, name, address, phoneNumber); 
@@ -11,27 +25,58 @@ public class Employee extends Patron {
     // METHODS
 
     // edit employee
+
+    /**
+     * Updates the employee's contact information.
+     * 
+     * @param name         new name for the employee
+     * @param address      new address for the employee
+     * @param phoneNumber  new phone number for the employee
+     */
     public void editEmployee(String name, String address, String phoneNumber) {
         super.editPatron(name, address, phoneNumber);
     }
 
     // delete employee
+
+    /**
+     * Deletes the employee's information and clears borrowed items.
+     */
     public void deleteEmployee() {
         super.deletePatron();
     }
 
     // retrieve employee based on their ID
+
+    /**
+     * Retrieves the employee's unique id.
+     * 
+     * @return the employee ID
+     */
     public String getEmployeeId() {
         return this.getId();
     }
 
     // type = employee
+
+    /**
+     * Gets the type of patron, which is "Employee" for this class.
+     * 
+     * @return the type of patron as a String ("Employee")
+     */
     @Override
     public String getType() {
         return "Employee";
     }
 
     // display values to user 
+
+    /**
+     * Returns a string representation of the employee, including contact 
+     * information and the employee ID.
+     * 
+     * @return a string with detailed information about the employee
+     */
     @Override
     public String toString() {
         return super.toString() + "\nEmployee ID: " + this.getId();
--- src/main/patrons/Student.java
+++ src/main/patrons/Student.java
@@ -1,9 +1,22 @@
 package main.patrons;
 // Student is a subclass of Patron
+
+/**
+ * Student class is a subclass of {@link Patron} that represents a student patron 
+ * in the library system. It inherits all attributes and methods from Patron.
+ */
 public class Student extends Patron {
 
     // CONSTRUCTOR
 
+    /**
+     * Constructs a new Student object with the specified attributes.
+     * 
+     * @param studentId    unique identifier for the student
+     * @param name         name of the student
+     * @param address      address of the student
+     * @param phoneNumber  phone number of the student
+     */
     public Student(String studentId, String name, String address, String phoneNumber) {
         // include all params of patron
         super("S" + studentId, name, address, phoneNumber);
@@ -12,27 +25,58 @@ public class Student extends Patron {
     // METHODS 
 
     // edit a student
+
+    /**
+     * Updates the student's contact information.
+     * 
+     * @param name         new name for the student
+     * @param address      new address for the student
+     * @param phoneNumber  new phone number for the student
+     */
     public void editStudent(String name, String address, String phoneNumber) {
         super.editPatron(name, address, phoneNumber);
     }
 
     // delete a student
+
+    /**
+     * Deletes the student's information and clears borrowed items.
+     */
     public void deleteStudent() {
         super.deletePatron();
     }
 
     // retrieve student based on their ID
+
+    /**
+     * Retrieves the student's unique id.
+     * 
+     * @return the student ID
+     */
     public String getStudentId() {
         return this.getId();
     }
 
     // type = student
+
+    /**
+     * Gets the type of patron, which is "Student" for this class.
+     * 
+     * @return the type of patron as a String ("Student")
+     */
     @Override
     public String getType() {
         return "Student";
     }
 
     // display values to user 
+
+    /**
+     * Returns a string representation of the student, including contact 
+     * information and the student ID.
+     * 
+     * @return a string with detailed information about the student
+     */
     @Override
     public String toString() {
         return super.toString() + "\nStudent ID: " + this.getId();
